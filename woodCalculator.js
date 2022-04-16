1fef24887190b207717a5b4802d3c1c2bfef1fdc
atikfaisalemon <atikfaisalemon@gmail.com>
2022-04-17 01:09:14 +0600

    wood calculator done

---
+++ woodCalculator.js
@@ -0,0 +1,12 @@
+let chair = 3;
+let bed = 15;
+
+function woodCalculator(tchair, tbed){
+    tchair = tchair *chair;
+    tbed = tbed * bed;
+    result = tchair+tbed;
+    return result;
+
+}
+ total = woodCalculator(5,2);
+console.log(total);
